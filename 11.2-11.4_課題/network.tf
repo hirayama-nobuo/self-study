40c355666f8153601d457e579216eb269b291609
平山信男 <hirayama.nobuo.isid.co.jp>
2019-11-04 09:53:31 +0900

    network.tfを修正

--- 11.2-11.4_課題/network.tf
+++ 11.2-11.4_課題/network.tf
@@ -1,73 +1,82 @@
 resource "aws_vpc" "vpc" {
-    cidr_block = "10.1.0.0/16"
-    instance_tenancy = "default"
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
 
-    tags = {
-      Name = "hirayama-test-vpc"
-    }
+  tags = {
+    Name = "hirayama-test-vpc"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
-    vpc_id = "aws_vpc.vpc.id"
-    cidr_block = "10.1.10.0/24"
-    availability_zone = "ap-northeast-1a"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "ap-northeast-1a"
 
-    tags{
-        Name = "hirayama-test-PnublicSubnet-1a"
-    }
+  tags {
+    Name = "hirayama-test-PublicSybnet-1a"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
-    vpc_id = "aws_vpc.vpc.id"
-    cidr_block = "10.1.110.0/24"
-    availability_zone = "ap-northeast-1a"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "10.1.110.0/24"
+  availability_zone = "ap-northeast-1a"
 
-    tags{
-        Name = "hirayama-test-PrivateSubnet-1a"
-    }
+  tags {
+    Name = "hirayama-test-PrivateSubnet-1a"
+  }
 }
 
 resource "aws_internet_gateway" "internet_gateway" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = "${aws_vpc.vpc.id}"
 }
 
-resouce "aws_route_table" "public" {
-    vpc_id = "aws_vpc.vpc.id"
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
 }
 
-resouce "aws_route" "public" {
-    route_table_id = "aws_route_table.public.id"
-    gateway_id = "aws_internet_gateway.internet_gateway.id"
-    destination_cidr_block = "0.0.0.0/0"
+resource "aws_route" "public" {
+  route_table_id         = "${aws_route_table.public.id}"
+  gateway_id             = "${aws_internet_gateway.internet_gateway.id}"
+  destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public"{
-    subnet_id = "aws_subnet.public_subnet.id"
-    route_table_id = "aws_route_table.public.id"
+resource "aws_route_table_association" "public" {
+  subnet_id      = "${aws_subnet.public_subnet.id}"
+  route_table_id = "${aws_route_table.public.id}"
 }
 
-resouce "aws_route_table" "private" {
-    vpc_id = "aws_vpc.vpc.id"
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
 }
 
-resource "aws_route_table_association" "private"{
-    subnet_id = "aws_subnet.private_subnet.id"
-    route_table_id = "aws_route_table.private.id"
+resource "aws_route_table_association" "private" {
+  subnet_id      = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.private.id}"
 }
 
 resource "aws_eip" "nat_gateway" {
-    vpc = "true"
-    depeonds_on = [aws_internet_gateway.internet_gateway]
+  vpc        = true
+  depends_on = ["aws_internet_gateway.internet_gateway"]
 }
 
 resource "aws_nat_gateway" "nat_gateway" {
-    allocation_id = "aws_eip.nat_gateway.id"
-    subnet_id = "aws_subnet.public_subnet.id
-    depends_on = [aws_internet_gateway.internet_gateway]
+  allocation_id = "${aws_eip.nat_gateway.id}"
+  subnet_id     = "${aws_subnet.public_subnet.id}"
+  depends_on    = ["aws_internet_gateway.internet_gateway"]
 }
 
 resource "aws_route" "private" {
-    route_table_id = "aws_route_table.private.id"
-    nat_gateway_id = "aws_nat_gateway.nat_gateway.id"
-    destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_gateway.id}"
+  destination_cidr_block = "0.0.0.0/0"
 }
+
+module "test_sg" {
+  source      = "./security_group"
+  name        = "hirayama-test-sg"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  port        = 80
+  cidr_blocks = ["106.154.130.252/32"]
+}
+
